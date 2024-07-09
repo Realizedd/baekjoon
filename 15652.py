d582258ad7f55075e1660cf6e8c6a32334fd3b61
Jungwoo Im <TheMCman12@gmail.com>
2024-07-09 16:21:06 +0900

    Create 15652.py

---
+++ 15652.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+
+def dfs(cnt, n, s):
+    for i in range(n, N + 1):
+        if cnt - 1 == 0:
+            print(s + str(i))
+        else:
+            dfs(cnt - 1, i, s + str(i) + ' ')
+
+dfs(M, 1, '')
